1ec0b9c366c28ba7429d2df683f38e5fd676f394
kunzangchophel <kunzangchophel362@gmail.com>
2026-09-18 12:02:02 +0600

    Unit IV Lab 3: PostgreSQL connection, Restaurant and MenuItem models, restaurant-specific menus

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,13 @@ const menuController = require('../controllers/menuController');
 const orderController = require('../controllers/orderController');
 
 router.post('/orders', orderController.createOrder);
+router.get('/restaurants/:id/menu', menuController.getMenuByRestaurant);
 router.get('/', homeController.getHome);
 router.get('/about', aboutController.getAbout);
-router.get('/menu', menuController.getMenu);
+
 
 module.exports = router;
+
 
 
 
